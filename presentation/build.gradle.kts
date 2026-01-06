27e6db3b8b917ed0bfa0e1289d456bdd180177cd
Saalim Quadri <danascape@gmail.com>
2026-01-06 11:35:28 +0530

    chore: Bump version to 1.0.6 (7)
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdk = 23
         targetSdk = 36
 
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         setProperty("archivesBaseName", "Messages-v${versionName}")
 
