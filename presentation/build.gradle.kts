2ae975e4078724d4b3aa707e6889a190343a010c
Saalim Quadri <danascape@gmail.com>
2026-01-12 12:21:48 +0530

    chore: Bump version to 1.0.7 (8)
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         minSdk = 23
         targetSdk = 36
 
-        versionCode = 7
-        versionName = "1.0.6"
+        versionCode = 8
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
